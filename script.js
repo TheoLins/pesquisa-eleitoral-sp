6d118a609c99b8450e224ffe66c2af6709bb0063
TheoLins Oficial <thelmolinhares@gmail.com>
2026-09-19 23:48:05 -0300

    Refactor candidate data structure and vote logic
    nova atualização

--- script.js
+++ script.js
@@ -18,8 +18,8 @@ document.addEventListener("DOMContentLoaded", () => {
         senador1: { 
             titulo: "Senador (1ª vaga)", 
             lista: { 
-                "222": { nome: "André do Prado (PL)", fotoUrl: "https://diariodejacarei.com.br/images/a/6059/b2ap3_large_Plenrio-Andr-do-Prado-site.jpg" }, 
-                "400": { nome: "Simode Tebet (PSB)", fotoUrl: "https://www12.senado.leg.br/noticias/materias/2022/02/15/simone-tebet-se-despede-da-lideranca-da-bancada-feminina-e-cobra-politicas-em-defesa-das-mulheres/20220215_01036jr.jpg/" } 
+                "123": { nome: "Ricardo Oliveira (PLOG)", fotoUrl: "https://unsplash.com" }, 
+                "789": { nome: "Sandra Melo (PTEC)", fotoUrl: "https://unsplash.com" } 
             } 
         },
         senador2: { 
