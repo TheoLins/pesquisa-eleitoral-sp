fb765867900681269a3604429d53daa1907eb884
TheoLins Oficial <thelmolinhares@gmail.com>
2026-09-24 20:34:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,43 +6,43 @@ function newFunction() {
             federal: {
                 titulo: "Deputado Federal",
                 lista: {
-                    "2208": { nome: "Mario Farias (PL)", fotoUrl: "https://camara.leg.br" },
-                    "2200": { nome: "José Augusto Rosa (PL)", fotoUrl: "https://camara.leg.br" }
+                    "2208": { nome: "Mario Farias (PL)", fotoUrl: "https://www.camara.leg.br/internet/deputado/bandep/pagina_do_deputado/220655.jpg" },
+                    "2200": { nome: "José Augusto Rosa (PL)", fotoUrl: "https://www.camara.leg.br/internet/deputado/bandep/pagina_do_deputado/178829.jpg" }
                 }
             },
             estadual: {
                 titulo: "Deputado Estadual",
                 lista: {
-                    "22090": { nome: "Nina Braga (PL)", fotoUrl: "https://ninabraga.com.br" },
-                    "22422": { nome: "Paulo Mansur (PL)", fotoUrl: "https://al.sp.gov.br    " }
+                    "22090": { nome: "Nina Braga (PL)", fotoUrl: "https://ninabraga.com.br/wp-content/uploads/2025/05/IMG_2050-min.jpg" },
+                    "22422": { nome: "Paulo Mansur (PL)", fotoUrl: "https://www3.al.sp.gov.br/repositorio/noticia/N-03-2024/fg320581.jpg" }
                 }
             },
             senador1: {
                 titulo: "Senador (1ª vaga)",
                 lista: {
-                    "111": { nome: "Guilherme Derrite (PP)", fotoUrl: "https://brasildefato.com.br" },
-                    "232": { nome: "Soninha Francine (Cidadania)", fotoUrl: "https://brasildefato.com.br" }
+                    "111": { nome: "Guilherme Derrite (PP)", fotoUrl: "https://www.brasildefato.com.br/wp-content/uploads/2025/11/img20251111183123940.jpg" },
+                    "232": { nome: "Soninha Francine (Cidadania)", fotoUrl: "https://www.brasildefato.com.br/wp-content/uploads/2026/09/soninha-6d3db0ec.webp" }
                 }
             },
             senador2: {
                 titulo: "Senador (2ª vaga)",
                 lista: {
-                    "222": { nome: "André do Prado (PL)", fotoUrl: "https://diariodejacarei.com.br" },
+                    "222": { nome: "André do Prado (PL)", fotoUrl: "https://diariodejacarei.com.br/images/a/6059/b2ap3_large_Plenrio-Andr-do-Prado-site.jpg" },
                     "400": { nome: "Simone Tebet (PSB)", fotoUrl: "https://senado.leg.br" }
                 }
             },
             governador: {
                 titulo: "Governador",
                 lista: {
-                    "10": { nome: "Tarcísio de Freitas (Republicanos)", fotoUrl: "https://gettyimages.com" },
-                    "13": { nome: "Fernando Haddad (PT)", fotoUrl: "https://gettyimages.com" }
+                    "10": { nome: "Tarcísio de Freitas (Republicanos)", fotoUrl: "https://media.gettyimages.com/id/2275918993/pt/foto/tarcisio-de-freitas-governor-of-so-paulo-speaks-during-a-campaign-launch-for-guilherme-derrite.jpg?s=594x594&w=gi&k=20&c=BaO1cmWaNTMIiMy8KKyg_JmcLK3HNsJB_Qz9rDH5qoQ=" },
+                    "13": { nome: "Fernando Haddad (PT)", fotoUrl: "https://media.gettyimages.com/id/2248000821/pt/foto/the-president-of-brazil-luiz-inacio-lula-da-silva-and-the-minister-of-finance-fernando-haddad.jpg?s=594x594&w=gi&k=20&c=_WEstkDzsPylqKA4tjMH_UBV2QxKACgbFVuGsr98my8=" }
                 }
             },
             presidente: {
                 titulo: "Presidente da República",
                 lista: {
-                    "22": { nome: "Fávio Bolsonaro (PL)", fotoUrl: "https://gettyimages.com" },
-                    "13": { nome: "Lula Inácio da Silva (PT)", fotoUrl: "https://gettyimages.com" }
+                    "22": { nome: "Fávio Bolsonaro (PL)", fotoUrl: "https://media.gettyimages.com/id/2281624229/pt/foto/brazilian-senator-and-presidential-hopeful-flavio-bolsonaro-presents-a-proposal-to-combat.jpg?s=594x594&w=gi&k=20&c=jcvUpIOeE_7MNr4Johfl9KDwGqgLTP2ubpIwkxR2jq4=" },
+                    "13": { nome: "Lula Inácio da Silva (PT)", fotoUrl: "https://media.gettyimages.com/id/2185130946/pt/foto/rio-de-janeiro-brazil-president-of-brazil-luis-in%C3%A1cio-lula-da-silva-gestures-during-the.jpg?s=594x594&w=gi&k=20&c=j5b8c1sbrwNJMZYRBqNr5-MD8iefJY6mdkaSrhmOWXQ=" }
                 }
             }
         };
